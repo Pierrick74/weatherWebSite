6c8249aeb52065b61baedc92cd4d2baa41326d5c
pierrick <pierrickviret@gmail.com>
2025-05-12 12:24:11 +0200

    feat: implement city selection functionality with checkbox and delete

--- assets/js/feedScript.js
+++ assets/js/feedScript.js
@@ -1,20 +1,29 @@
 import { getweatherData, getCityCoordinate } from "./APIScript.js";
 
 let datas = [];
+let isSelectionActive = false;
 
 // create a fonction to create data with list of city
 let cityList = [
     {"lat":"45,899930",
         "lon": "6,12874",
+        "new": false,
+        "name": "AnnecyTest",
     },
     {"lat":"45,78844",
         "lon": "6,09840",
+        "new": false,
+        "name": "City2",
     },
     {"lat":"45,94261",
         "lon": "6,42611",
+        "new": false,
+        "name": "City3",
     },
     {"lat":"45,90289",
         "lon": "6,42386",
+        "new": false,
+        "name": "City4",
     }
 ];
 
@@ -22,22 +31,20 @@ async function refreshData() {
     for (const city of cityList) {
         try {
             const cityData = await getweatherData(city); // Attendre les données pour chaque ville
-            console.log("recupere data");
-            console.log(cityData);
             datas.push({
                 title: cityData.name,
                 temperature: cityData.main.temp,
                 imageUrl: `https://openweathermap.org/img/wn/${cityData.weather[0].icon}@2x.png`,
+                new: city.new,
             });
         } catch (error) {
             console.error(`Erreur lors de la récupération des données pour ${city}:`, error);
         }
     }
-    console.log(datas);
 }
 
 // create a function to create card elements
-function createCardElement(title, temperature, imageUrl) {
+function createCardElement(title, temperature, imageUrl, newCity) {
     
     const card = document.createElement("div");
     card.classList.add("card");
@@ -53,12 +60,19 @@ function createCardElement(title, temperature, imageUrl) {
     
     const cardTemperature = document.createElement("p");
     cardTemperature.textContent = temperature + "°C";
+
+    const cardSelection = document.createElement("input");
+    cardSelection.type = "checkbox"
+    cardSelection.id = title
     
     cardContainer.appendChild(cardTitle);
     cardContainer.appendChild(cardTemperature);
     
     card.appendChild(img);
     card.appendChild(cardContainer);
+    if(newCity && isSelectionActive) {
+        card.appendChild(cardSelection);
+    }
     return card;
 }
 
@@ -69,9 +83,7 @@ function displayData(datas) {
     resetData();
 
     datas.forEach((data) => {
-        console.log("affiche card");
-        console.log(data);
-        const card = createCardElement(data.title, data.temperature, data.imageUrl);
+        const card = createCardElement(data.title, data.temperature, data.imageUrl, data.new);
         cardContainer.appendChild(card);
     });
 }
@@ -101,11 +113,38 @@ async function addCity() {
         let city = await getCityCoordinate(input)
         cityList.push(
             {"lat":city.lat,
-            "lon": city.lon
+            "lon": city.lon,
+            "new": true,
+            "name": input,
         });
         displayInfo();
 
     } catch (error) {
-        console.error(`Erreur lors de la récupération des données pour ${city}:`, error);
+        console.error(`Erreur lors de la récupération des données pour ${input}:`, error);
     }
 }
+
+//-------------selection elements ----------------
+document.getElementById("selection").addEventListener("click", toggleIsSelection);
+
+function toggleIsSelection() {
+    if(isSelectionActive) {
+        isSelectionActive = false;
+        document.getElementById("selection").textContent = "Selection";
+        deleteSelectedCity() ;
+    } else {
+        isSelectionActive = true;
+        document.getElementById("selection").textContent = "Effacer la selection";
+    }
+    displayInfo();
+}
+
+function deleteSelectedCity() {
+    let newCityList = [];
+    const selectedCities = document.querySelectorAll('input[type="checkbox"]:checked');
+    selectedCities.forEach((city) => {
+        const cityName = city.id;
+        newCityList = cityList.filter((city) => city.name !== cityName);
+    });
+    cityList = newCityList;
+}   
