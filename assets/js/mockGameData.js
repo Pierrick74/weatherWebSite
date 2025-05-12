34ab0c53632bff5364b8fafb33f63c85ba255fb2
pierrick <pierrickviret@gmail.com>
2025-05-12 15:08:09 +0200

    add game html page

---
+++ assets/js/mockGameData.js
@@ -0,0 +1,17 @@
+const jsonGameDatas=[
+    {
+      "name": "sunrise",
+      "description": "A toi de trouver les bons soleils !",
+      "dificulté": 1,
+      "pairs": 5,
+      "img": [
+        "https://openweathermap.org/img/wn/10d@2x.png",
+        "https://openweathermap.org/img/wn/01d@2x.png",
+        "https://openweathermap.org/img/wn/13d@2x.png",
+        "https://openweathermap.org/img/wn/50d@2x.png",
+        "https://openweathermap.org/img/wn/04d@2x.png",
+      ],
+    }
+  ];
+  
+  export default jsonGameDatas;
